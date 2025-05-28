1d364c1f65ec23b13e90d015b6eaadbf96c93855
Pragati261999 <pragati1999tiwari@gmail.com>
2025-05-28 17:24:26 +0530

    server file api

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
 
+
+dotenv.config();
 const app = express()
-
+const PORT = process.env.PORT
 app.get("/",(req, res)=>{
     res.send("Hello world");
 })
 
-app.listen(5001, ()=>{
-    console.log("Server is running on this port 5001");
+app.listen(PORT, ()=>{
+    console.log(`Server is running on this port ${PORT} `);
 })
