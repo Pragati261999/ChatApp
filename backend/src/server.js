5cff47661885a1aa88a4aaf3e1eebf6e5adb682b
Pragati261999 <pragati1999tiwari@gmail.com>
2025-06-01 22:55:19 +0530

    mongoose connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express()
 const PORT = process.env.PORT
@@ -12,4 +13,5 @@ app.use('/api/auth', authRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on this port ${PORT} `);
+    connectDB();
 })
