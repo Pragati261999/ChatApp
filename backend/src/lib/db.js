bf9dcae76ca6f213d661adbd58370dcc254a0b51
Pragati261999 <pragati1999tiwari@gmail.com>
2025-06-02 22:25:32 +0530

    db connected

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,11 +1,23 @@
+// import mongoose from "mongoose";
+
+// export const connectDB =  async ()=>{
+//     try{
+//         await mongoose.connect(process.env.MONGO_URL);
+//         console.log(`Mongo connection :${connectDB.connection.host} `);
+//     } catch (error){
+//         console.log(error);
+//         process.exit(1);  //failure  code
+//     }
+// }
+
 import mongoose from "mongoose";
 
-export const connectDB =  async ()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log(`Mongo connection :${connectDB.connection.host} `);
-    } catch (error){
-        console.log(error);
-        process.exit(1);  //failure  code
-    }
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL); // or MONGO_URI if you renamed it
+    console.log(`MongoDB connected: ${mongoose.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
