1d4383525ccf3bc85d210e909f9ff58665cb9123
Pragati261999 <pragati1999tiwari@gmail.com>
2025-06-02 23:00:35 +0530

    user schea

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -23,6 +23,7 @@ const User = mongoose.model("User", userSchema);
 
 userSchema.pre("save", async function name(next) {
     try {
+        if(!this.isModified("password")) return next();
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
         next();
