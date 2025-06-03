aa9613f377b23c0e4e2f31361304f35618a4d095
Pragati261999 <pragati1999tiwari@gmail.com>
2025-06-03 22:51:02 +0530

    signup api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,12 @@
 export async function signup (req, res){
     const {email, password,fullName} =  req.body;
+    try{
+if(!email || !password || !fullName ){
+    return res.status(400).json({ message: "All fields ar required"});
+}
+    }catch(error){
+
+    }
     res.send("signup Routes");
 }
 export async function login (req, res){
