cbaf08ead9cdff2673dfea4bbe1daa1c14445573
Pragati261999 <pragati1999tiwari@gmail.com>
2025-06-11 22:54:10 +0530

    api of user

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
-
+import { Profiler } from "react";
+import User from '../models/User.js';
+import jwt from "jsonwebtoken";
 
 
 export async function signup(req, res) {
@@ -21,6 +23,16 @@ export async function signup(req, res) {
 
         }
         const idx = Math.floor(Math.random()*100)+1; 
+
+        const randomavtar  =  `https://avatar.iran.liara.run/public/${idx}.png`;
+        const newuser = new User.create({
+            email,fullName, password, ProfilePic:randomavtar
+        });
+        const token =  jwt.sign({userId:newuser._id}, process.env.JWT_SECRET, {
+            expireIn: "7d"
+        })
+
+        // });
 
     } catch (error) {
 
