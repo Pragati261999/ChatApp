4f486bb3baaaa00a2eacc634d72da882c7d828ac
Pragati261999 <pragati1999tiwari@gmail.com>
2025-06-16 22:55:03 +0530

    login api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,49 +4,50 @@ import jwt from "jsonwebtoken";
 
 
 export async function signup(req, res) {
-    const { email, password, fullName } = req.body;
-    try {
-        if (!email || !password || !fullName) {
-            return res.status(400).json({ message: "All fields ar required" });
-        }
-        if (password.lenth < 6) {
-            return res.status(400).json({ message: "must be 6 digits " });
-
-        }
-        const emailFormat = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-        if (!emailFormat.test(email)) {
-            return res.status(400).json({ message: "invailid mail" });
-        }
-        const existemail = await User.findOne({ email });
-        if (existemail) {
-            return res.status(400).json({ message: "already exist!" });
-
-        }
-        const idx = Math.floor(Math.random() * 100) + 1;
-
-        const randomavtar = `https://avatar.iran.liara.run/public/${idx}.png`;
-        const newuser = new User.create({
-            email, fullName, password, ProfilePic: randomavtar
-        });
-        const token = jwt.sign({ userId: newuser._id }, process.env.JWT_SECRET, {
-            expireIn: "7d"
-        })
-        res.cookie("jwt", token, {
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-            httpOnly: true,    // prevent xss attacks, 
-            sameSite: "strict",
-            secure: process.env.NODE_ENV === "production"
-        })
-        res.status(201).json({ success: true, user: newuser })
-        // });
-
-    } catch (error) {
+  const { email, password, fullName } = req.body;
+  try {
+    if (!email || !password || !fullName) {
+      return res.status(400).json({ message: "All fields ar required" });
+    }
+    if (password.lenth < 6) {
+      return res.status(400).json({ message: "must be 6 digits " });
 
     }
-    res.send("signup Routes");
+    const emailFormat = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    if (!emailFormat.test(email)) {
+      return res.status(400).json({ message: "invailid mail" });
+    }
+    const existemail = await User.findOne({ email });
+    if (existemail) {
+      return res.status(400).json({ message: "already exist!" });
+
+    }
+    const idx = Math.floor(Math.random() * 100) + 1;
+
+    const randomavtar = `https://avatar.iran.liara.run/public/${idx}.png`;
+    const newuser = new User.create({
+      email, fullName, password, ProfilePic: randomavtar
+    });
+    const token = jwt.sign({ userId: newuser._id }, process.env.JWT_SECRET, {
+      expireIn: "7d"
+    })
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,    // prevent xss attacks, 
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production"
+    })
+    res.status(201).json({ success: true, user: newuser })
+    // });
+
+  } catch (error) {
+
+  }
+  res.send("signup Routes");
 }
 export async function login(req, res) {
 }
-export async function logout(req, res) {
-    res.send("logout Routes");
+export function logout(req, res) {
+  res.clearCookie("jwt");
+  res.status(200).json({ success: true, message: "Logout successful" });
 }
