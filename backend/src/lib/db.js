d16063c52bef726495f3f68975e2e8117716f92e
Pragati261999 <pragati1999tiwari@gmail.com>
2025-06-20 23:33:12 +0530

    auth correction

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -14,7 +14,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL); // or MONGO_URI if you renamed it
+    await mongoose.connect(process.env.MONGO_URI); // or MONGO_URI if you renamed it
     console.log(`MongoDB connected: ${mongoose.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection error:", error);
