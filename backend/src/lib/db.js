a78f49e50ca81b1e81f827b81eeeec33febf86d5
Pragati261999 <pragati1999tiwari@gmail.com>
2025-06-23 09:45:13 +0530

    remove

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,14 +1,3 @@
-// import mongoose from "mongoose";
-
-// export const connectDB =  async ()=>{
-//     try{
-//         await mongoose.connect(process.env.MONGO_URL);
-//         console.log(`Mongo connection :${connectDB.connection.host} `);
-//     } catch (error){
-//         console.log(error);
-//         process.exit(1);  //failure  code
-//     }
-// }
 
 import mongoose from "mongoose";
 
