3ed8486c5615afa5cc46ee81e63afaceff1619e5
Pragati261999 <pragati1999tiwari@gmail.com>
2025-07-01 22:58:15 +0530

    server .js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-
-import authRoutes from "./routes/auth.route.js"
 import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
+
 dotenv.config();
 const app = express()
 const PORT = process.env.PORT;
