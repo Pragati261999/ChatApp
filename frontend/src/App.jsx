f5bd8356c3931d71eb7987af3a9892583fd292bc
Pragati261999 <pragati1999tiwari@gmail.com>
2025-07-04 23:56:06 +0530

    query imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,11 +12,22 @@ import ChatPage from "./pages/ChatPage";
 import CallPage from "./pages/CallPage";
 import OnboardingPage from "./pages/OnboardingPage";
 import toast, { Toaster } from "react-hot-toast";
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: "todos",
+    queryFn: async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/todos")
+      const data = res.json();
+      return data;
+    },
+  });
+  console.log("tods data: ", data);
   return (
     <div className='h-screen' data-theme="forest">
-      <button onClick={() =>{toast.success('Hello World!')}}>Create a toast</button>
+      <button onClick={() => { toast.success('Hello World!') }}>Create a toast</button>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/signup" element={<SignupPage />}></Route>
