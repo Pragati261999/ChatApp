7566fc410d11ff95fbeaf994bce48644b44c61bc
Pragati261999 <pragati1999tiwari@gmail.com>
2025-07-04 16:26:30 +0530

    import page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,26 @@
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
-import { Routes } from "react-router";
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import NotificationPage from "./pages/NotificationPage";
+import ChatPage from "./pages/ChatPage";
+import CallPage from "./pages/CallPage";
+import OnboardingPage from "./pages/OnboardingPage";
 
 function App() {
   return (
     <div className='h-screen' data-theme="forest">
       <Routes>
-        
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/signup" element={<SignupPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/onboard" element={<OnboardingPage />}></Route>
+        <Route path="/notification" element={<NotificationPage />}></Route>
+        <Route path="/chat" element={<ChatPage />}></Route>
+        <Route path="/call" element={<CallPage />}></Route>
       </Routes>
     </div>
   );
