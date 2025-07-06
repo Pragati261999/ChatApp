7758b72574fb78cae55d2e004cbcffad77d93fa3
Pragati261999 <pragati1999tiwari@gmail.com>
2025-07-06 16:08:58 +0530

    use cors in backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,10 @@ dotenv.config();
 const app = express()
 const PORT = process.env.PORT;
 // app.use('/api/auth', authRoutes);
-app.use(cors());
+app.use(cors({
+    origin: "http:/localhost:5173",
+    credentials: true   // allow send cookies
+}));
 app.use(express.json());
 app.use(cookieParser());
 
