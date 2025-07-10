dd08f44f7915745c87a6f3415b03b7d527a91622
Pragati261999 <pragati1999tiwari@gmail.com>
2025-07-10 22:43:24 +0530

    logo

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,16 +1,30 @@
 import React, { useState } from 'react'
+import { ShipWheelIcon } from "lucide-react"
 
 const SignupPage = () => {
-  const [signup ,setsignupData] =  useState({
+  const [signup, setsignupData] = useState({
     fullName: "",
     email: "",
-    password:"",
-  }) ;
-  const handlesubmit = (e) =>{
+    password: "",
+  });
+  const handlesubmit = (e) => {
     e.preventDefault()
   }
   return (
-    <div className='h-screen flex items-center justify-center p-4 sm:p-6 md:p-8' data-theme="forest">SignupPage</div>
+    <div className='h-screen flex items-center justify-center p-4 sm:p-6 md:p-8' data-theme="forest">
+      <div className='border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden'>
+        {/* signup form - left sode  */}
+        <div className='w-full lg:w-1/2 p-4 sm:p-8 flex flex-col'>
+          {/* logo */}
+          <div className='mb-4 flex items-center justify-start gap-2'>
+            <ShipWheelIcon className="size-9 " />
+            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
+              Togetia
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
