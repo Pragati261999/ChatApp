9f73b5b6bc9c00814c2ce6def148fb9b93f5da6e
Pragati261999 <pragati1999tiwari@gmail.com>
2025-07-12 13:00:04 +0530

    login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 
 function LoginPage() {
-    const [loginData, setLoginData] = useState({
+  const [loginData, setLoginData] = useState({
     email: "",
     password: "",
   });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    loginMutation(loginData);
+  };
   return (
-    <div>LoginPage</div>
+    <div
+      className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8"
+      data-theme="forest"
+    >
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+        {/* LOGIN FORM SECTION */}
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+          {/* LOGO */}
+          <div className="mb-4 flex items-center justify-start gap-2">
+            <ShipWheelIcon className="size-9 text-primary" />
+            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
+              Streamify
+            </span>
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
